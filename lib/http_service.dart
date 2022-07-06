9469f18f88f5398fe16c34ec9e1b18397587b589
Anant Kanchan <anant4265@gmail.com>
2022-07-07 00:29:20 +0530

    raw info display from api

---
+++ lib/http_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:http_service/post_model.dart';
+
+class HttpService {
+  //final String postsUrl = "https://jsonplaceholder.typicode.com/posts";
+
+
+
+  Future<List<Post>> getPosts() async {
+    Response res = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
+
+
+    if(res.statusCode==200) {
+      List<dynamic> body = jsonDecode(res.body);
+
+      List<Post> posts = body.map((dynamic item) => Post.fromJson(item))
+          .toList();
+      return posts;
+    } else {
+      throw "Can't get posts.";
+    }
+  }
+}
